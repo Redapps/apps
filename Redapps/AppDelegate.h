a629ed913404ae320fe3f27d0a4988c34b24a413
Ryan Copley <ryanwebdev@gmail.com>
2013-06-30 22:33:44 -0400

    Empty project created

---
+++ Redapps/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  app
+//
+//  Created by Ryan Copley on 6/30/13.
+//  Copyright (c) 2013 Redapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
